a3b9a9bc835decf59db2775b6c79f32f4afe8587
tarafans <tarafans7@gmail.com>
2023-08-25 18:29:17 -0700

    Update contracts

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 abstract contract ERC4626 is ERC20 {
     IERC20 asset;
